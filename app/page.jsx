030e6dfb63e26cc9ce25810ad01618190c1c32da
Nazmul1582 <nazmulhasan1582@gmail.com>
2026-07-08 16:41:38 +0600

    initial setup

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="min-h-dvh flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-center">
+        Welcome To Root Kitchen!
+      </h1>
+    </div>
+  );
+}
