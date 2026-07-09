470efc11a8040233353d326be2c453ccc84d4c3a
Nazmul1582 <nazmulhasan1582@gmail.com>
2026-07-09 06:13:12 +0600

    create categories component and fetch dynamic data

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
+import Categories from "@/components/home/Categories";
 import Featured from "@/components/home/Featured";
-import Heritage from "@/components/home/Heritage";
+// import Heritage from "@/components/home/Heritage";
 import Hero from "@/components/home/Hero";
 import PhilosophyQuote from "@/components/home/PhilosophyQuote";
 import React from "react";
@@ -8,7 +9,8 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Heritage />
+      {/* <Heritage /> */}
+      <Categories />
       <Featured />
       <PhilosophyQuote />
     </>
--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "www.themealdb.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
