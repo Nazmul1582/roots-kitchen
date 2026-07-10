3bf8a59283406b6d52d183ef77752c56a70b7d3f
Nazmul1582 <nazmulhasan1582@gmail.com>
2026-07-10 16:19:42 +0600

    make the add to cart button client component and implement the onClick method

--- components/button/Button.jsx
+++ components/button/Button.jsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Button() {
+  const [isCart, setIsCart] = useState(false);
   return (
-    <button className="bg-orange-800 text-white px-5 py-2 rounded-full hover:bg-orange-900 transition text-sm md:cursor-pointer">
+    <button
+      onClick={() => setIsCart(true)}
+      disabled={isCart}
+      className="bg-orange-800 text-white px-5 py-2 rounded-full hover:bg-orange-900 transition text-sm md:cursor-pointer disabled:bg-stone-300 disabled:cursor-not-allowed"
+    >
       Add to cart
     </button>
   );
