04b02f9f28367fd1af1078f6c0aaa492934e5aba
Nazmul1582 <nazmulhasan1582@gmail.com>
2026-07-13 12:52:25 +0600

    recipe get and create using mongodb successfully

--- app/api/recipes/route.js
+++ app/api/recipes/route.js
@@ -1,17 +1,22 @@
-import { recipesDB } from "../route";
+import { connect } from "@/lib/dbConnect";
 
-export function GET(req) {
+const recipesCollection = connect("recipes");
+
+export async function GET(req) {
+  const result = await recipesCollection.find().toArray();
   return Response.json({
     status: 200,
     success: true,
     message: "Data served successfully",
-    data: recipesDB,
+    data: result,
   });
 }
 
 export async function POST(req) {
-  const { title, price, category } = await req.json();
-  if (!title || !price) {
+  const data = await req.json();
+  console.log(data);
+
+  if (!data.title || !data.price) {
     return Response.json({
       status: 400,
       success: false,
@@ -19,10 +24,12 @@ export async function POST(req) {
     });
   }
 
+  const newRecipe = await recipesCollection.insertOne(data);
+
   return Response.json({
     status: 201,
     success: true,
     message: "Recipe created successfully",
-    data: { title, price, category },
+    data: newRecipe,
   });
 }
