2d605483f8af380f9ea5b87d489164b0e859090b
Nazmul1582 <nazmulhasan1582@gmail.com>
2026-07-13 12:01:15 +0600

    create a post request for recipe

--- app/api/recipes/route.js
+++ app/api/recipes/route.js
@@ -4,6 +4,25 @@ export function GET(req) {
   return Response.json({
     status: 200,
     success: true,
+    message: "Data served successfully",
     data: recipesDB,
+  });
+}
+
+export async function POST(req) {
+  const { title, price, category } = await req.json();
+  if (!title || !price) {
+    return Response.json({
+      status: 400,
+      success: false,
+      message: "Title and Price are required",
+    });
+  }
+
+  return Response.json({
+    status: 201,
+    success: true,
+    message: "Recipe created successfully",
+    data: { title, price, category },
   });
 }
